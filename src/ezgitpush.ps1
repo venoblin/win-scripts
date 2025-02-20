05b3b470bf5897baad65760fc64653ad35c2f0df
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-02-19 22:54:03 -0500

    worked on script

--- src/ezgitpush.ps1
+++ src/ezgitpush.ps1
@@ -1,2 +1,9 @@
 # Gets current git branch and pushes code with a specified message
 
+$CurrentGitBranch = git symbolic-ref --short HEAD
+
+param(
+  [Parameter(Mandatory=$true)][string]$Message
+)
+
+echo $Message
