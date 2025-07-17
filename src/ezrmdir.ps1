aedf54e4b29fc55a6f104fa2f37abe3b885cc981
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-07-17 15:55:52 -0400

    finished ezrmdir script

--- src/ezrmdir.ps1
+++ src/ezrmdir.ps1
@@ -1,2 +1,11 @@
 # Easily removes directory and its subdirectories
 
+param (
+  [Parameter(Mandatory=$true)][string]$p
+)
+
+if (Test-Path -Path $p -PathType Container) {
+  Remove-Item -Path $p -Recurse -Force
+} else {
+  Write-Host "Error: This is not a directory!"
+}
