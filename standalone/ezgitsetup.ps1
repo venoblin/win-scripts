9f53ee78714c088b255f380923859aa5f5f7a08e
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-08-04 15:05:43 -0400

    created new script

--- standalone/ezgitsetup.ps1
+++ standalone/ezgitsetup.ps1
@@ -1,1 +1,17 @@
 # Sets up git
+
+param (
+  [Parameter(Mandatory=$true)][string]$n,
+  [Parameter(Mandatory=$true)][string]$e
+)
+
+git config --global user.name $n
+git config --global user.email $e
+
+git config --global init.defaultBranch main
+git config --global color.ui auto
+git config --global pull.rebase false
+
+ssh-keygen -t ed25519
+
+Write-Output "Success: id_ed25519.pub file created in you .ssh directory!"
